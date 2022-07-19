a11253a42043cd1fbc7c0a975bd670f937b5a036
KonceptD <kiranjayd@yahoo.com>
2022-07-19 09:30:47 +0200

    First commit. Starting over from experience, not scratch, after reading documentation, watching tutorials etc. Will see how this one fares and will merge if I make a breakthrough here

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// * TODO: 16x16 square divs using JS
-// * TODO: Append them to a parent container
-// * TODO: Convert divs to a grid
-// * TODO: Hover effect of a color change when the mouse passes through
+// TODO: 16x16 square divs using JS
+// TODO: Append them to a parent container
+// TODO: Convert divs to a grid
+// TODO: Hover effect of a color change when the mouse passes through
 // TODO: Button on top to prompt user for no. of squares - max 100 (must replace the old grid)
 
 
@@ -9,28 +9,39 @@ const container= document.getElementById("container")
 
 
 
-// function that creates the grid - Taken from StackOverflow
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.addEventListener('mouseover', function(e){
-            e.target.style.backgroundColor = 'black';
-        })
-        container.appendChild(cell).className = "grid-item"
-    };
-};
-makeRows (32, 32);
 
-// Adds the reset button, and event listener for it
-const reset = document.querySelector('#reset')
-function resetButton(rows, cols) {
-    while (container.firstChild) {
-        container.removeChild(container.lastChild);
-    }
-    makeRows(32,32);
-}
-reset.addEventListener('click', resetButton)
+
+
+
+
+
+
+
+// function that creates the grid - Taken from StackOverflow
+// function makeRows(rows, cols) {
+//     container.style.display = "grid";
+//     container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+//     container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+//     // container.style.setProperty('--grid-rows', rows);
+//     // container.style.setProperty('--grid-cols', cols);
+//     for (c = 0; c < (rows * cols); c++) {
+//         let cell = document.createElement("div");
+//         cell.addEventListener('mouseover', function(e){
+//             e.target.style.backgroundColor = 'black';
+//         })
+//         container.appendChild(cell).className = "grid-item"
+//     };
+// };
+// makeRows (16, 16);
+
+// // Adds the reset button, and event listener for it
+// const reset = document.querySelector('#reset')
+// function resetButton(rows, cols) {
+//     while (container.firstChild) {
+//         container.removeChild(container.lastChild);
+//     }
+//     makeRows(16,16);
+// }
+// reset.addEventListener('click', resetButton)
 
 
